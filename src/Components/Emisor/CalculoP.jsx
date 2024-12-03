3771fcc001f5eac503e78f624b1b81b59aa53ec9
Francisco Perez <franutn23@gmail.com>
2024-12-03 03:10:37 -0300

    add calc of quantity of parity bits and change the size of footer

--- src/Components/Emisor/CalculoP.jsx
+++ src/Components/Emisor/CalculoP.jsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 export default function CalculoP({binary, fijarP}){
-    
     const n = binary.length;
-
     const [p, setP] = useState(1);
-
     const [leftResult, setLeftResult] = useState(0);
-
     const [rightResult, setRightResult] = useState(0);
 
     useEffect(() => {
@@ -32,20 +29,65 @@ export default function CalculoP({binary, fijarP}){
     }
     
     return(
-        <section id="calcular-p">
-            <h2 className="cadena">{binary}</h2>
-            <h3>2<span className="exponential">p</span> ≥ n + p + 1</h3>
-            <h1 id="main-ecuation">2<span id="exponential-main" className="p">{p}</span>≥ {n} + <span className="p">{p}</span> + 1</h1>
-            <h1>{leftResult} ≥ {rightResult}</h1>
+        <section className="flex flex-col items-center justify-center bg-gray-900 p-4 min-w-full min-h-full">
+            <h1 className="font-bold text-5xl mb-4 text-violet-600">Calculo de P</h1>
+            <h2 className="text-white text-3xl mb-4">{binary}</h2>
+            <h3 className="text-white mb-2 text-4xl">
+                2
+                <span className="text-purple-500 text-4xl">
+                    <motion.span animate={{ scale: 1.5 }} className="font-bold align-super text-xl">p</motion.span>
+                </span> 
+                ≥ n + p + 1
+            </h3>
+            <motion.h1 
+                className="text-white text-4xl mb-2"
+                initial={{ opacity: 0, x: -50 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: 50 }}
+                transition={{ duration: 0.5 }}
+                key={p}
+            >
+                2
+                <span className="text-purple-500 text-4xl">
+                    <motion.span animate={{ scale: 1.5 }} className="font-bold align-super text-xl mx-1">{p}</motion.span>
+                </span> 
+                ≥ {n} + 
+                <span className="text-purple-500">
+                    <motion.span animate={{ scale: 1.5 }} className="font-bold text-4xl"> {p} </motion.span>
+                </span> 
+                + 1
+            </motion.h1>
+            <motion.h1 
+            className="text-white text-3xl mb-6"
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 50 }}
+            transition={{ duration: 0.5 }}
+            key={`${leftResult}-${rightResult}`}
+        >
+            <span className={`text-3xl mb-6 ${leftResult >= rightResult ? "text-green-500" : "text-red-500"}`}>{leftResult}</span> ≥ {rightResult}
+        </motion.h1>
             {
                 (reachP(p)) 
                 ?   
-                <div className="buttons-div">
-                    <button onClick={sumarUno}>Sumar 1</button>
-                    <button onClick={llegarP}>Llegar a p</button>
+                <div className="flex flex-wrap gap-4">
+                    <button 
+                        className="bg-violet-600 hover:bg-violet-700 text-white font-bold py-3 px-6 rounded"
+                        onClick={sumarUno}>
+                        Sumar 1
+                    </button>
+                    <button 
+                        className="bg-violet-600 hover:bg-violet-700 text-white font-bold py-3 px-6 rounded"
+                        onClick={llegarP}>
+                        Llegar a p
+                    </button>
                 </div> 
                 : 
-                <button onClick={()=> fijarP(p)}>Ir al cuadro</button>
+                <button 
+                    className="bg-violet-600 hover:bg-violet-700 text-white font-bold py-3 px-6 rounded"
+                    onClick={()=> fijarP(p)}>
+                    Ir a la Tabla
+                </button>
             }
         </section>
     )
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,10 +3,10 @@ export default function Footer() {
     return (
         <footer className="bg-gray-800 shadow mt-auto w-full">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-400 sm:text-center">© {date.getFullYear()} <a href="https://franperez.vercel.app" className="hover:underline">Francisco Miguel Perez</a></span>
-                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-400 sm:mt-0">
+                <span className="text-xl text-gray-400 sm:text-center">© {date.getFullYear()} <a href="https://franperez.vercel.app" className="hover:underline text-xl">Francisco Miguel Perez</a></span>
+                <ul className="flex flex-wrap items-center mt-3 text-lg font-medium text-gray-400 sm:mt-0">
                     <li>
-                        <p>Comunicaciones - 3K2 - Ing. Carrasco</p>
+                        <p className="text-xl">Comunicaciones - 3K2 - Ing. Carrasco</p>
                     </li>
                 </ul>
             </div>
