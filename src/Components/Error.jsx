ae20a3c6cf6c4e77d9cdf2546e8c63abc83addb7
Francisco Perez <franutn23@gmail.com>
2024-12-03 03:30:26 -0300

    add error page

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
 
@@ -5,8 +6,17 @@ export default function ErrorView() {
   return (
     <>
       <Header></Header>
-      <section>
-        <h1>Hubo un error.</h1>
+      <section className="bg-gray-900 min-w-full min-h-full flex justify-center items-center">
+        <div className="flex flex-col items-center gap-6">
+          <h1 className="text-4xl text-violet-500">Hubo un error catastrófico...</h1>
+          <Link to={`/`}>
+            <button
+              className="bg-violet-600 hover:bg-violet-700 text-white font-bold py-3 px-6 rounded"
+            >
+              Volver al Inicio
+            </button>
+          </Link>
+        </div>
       </section>
       <Footer></Footer>
     </>
